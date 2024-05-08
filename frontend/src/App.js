2580a3e754ecec81eb6af15bfb181edcfa81c3b6
gauricb <gbarar@umass.edu>
2024-05-08 19:40:56 -0400

    added login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MainNavbar from "./components/Navbar.js";
 import CodeTranslator from "./components/CodeTranslate.js";
 import Home from "./pages/Home.js";
+import Login from "./pages/Login.js";
 
 function App() {
 	return (
@@ -12,6 +13,7 @@ function App() {
 				<Routes>
 					<Route path="/translate" element={<CodeTranslator />} />
 					<Route path="/history" element={<Home />} />
+					<Route path="/login" element={<Login />} />
 					{/* <Route path="/translate" element={<CodeTranslator />} /> */}
 				</Routes>
 			</div>
