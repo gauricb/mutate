0f1c41394f9a7b031f9760fa3ca34fe70dd6a768
Matthew Richardson <97010005+06tron@users.noreply.github.com>
2024-05-12 18:56:39 -0400

    Starts work on test.py with evaluate_func()

---
+++ backend/test.py
@@ -0,0 +1,18 @@
+from langchain.prompts import load_prompt
+from langchain.chains import LLMChain
+from langchain_openai import ChatOpenAI
+
+def evaluate_func(
+    model_name: str, target_lang: str, correct_translation: str, generated_translation: str
+):
+    prompt = load_prompt("prompts/evaluator.yaml")
+    llm = ChatOpenAI(model_name=model_name, temperature=0)
+    chain = LLMChain(llm=llm, prompt=prompt, verbose=False)
+    return chain.invoke(
+        {
+            "target_lang": target_lang,
+            "source_code": source_code,
+            "correct_translation": correct_translation,
+            "generated_translation": generated_translation,
+        }
+    )["text"].lower() in ['true', '"true"']
